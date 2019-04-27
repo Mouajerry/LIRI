c02edc1d0ca1b7ce53b412c0a232bb6ad1096aa6
Jerry Moua <jerrymoua@gmail.com>
2019-04-26 19:41:18 -0500

    updating keys.js

--- Liri/keys.js
+++ Liri/keys.js
@@ -1,6 +1,14 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "f3d28a4b52a541beb5bd8752ccd842b4",
+  secret: "4dfd2f39951d4a318470c46e8304796b"
 };
+
+exports.bandsintown = {
+  id: process.env.BANDSINTOWN_ID
+}
+
+exports.omdb = {
+  id: process.env.OMDB_ID
+}
