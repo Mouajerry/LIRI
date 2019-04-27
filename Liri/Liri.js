9b454ad8d963819e303bc49e106527692a195feb
Jerry Moua <jerrymoua@gmail.com>
2019-04-26 20:37:07 -0500

    changing words

--- Liri/Liri.js
+++ Liri/Liri.js
@@ -23,3 +23,28 @@ let ombd = (keys.ombd);
 // Bandsintown API
 let bandsintown = (keys.bandsintown);
 
+var userinput = process.argv[2];
+var userQuery = process.argv.slice(3).join(" ");
+
+// command questions
+function userCommand(userinput, userQuery) {
+
+    switch (userinput) {
+        case "consert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-what-it-says":
+            doThis();
+            break;
+        default:
+            console.log("I don't know, Please ask again or another question.")    
+            break;
+    }
+}
+
